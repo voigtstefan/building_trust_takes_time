2c95b64caf3c56ea190e6c4e5f5f893a891844f3
voigtstefan <stefan.voigt@econ.ku.dk>
2023-01-17 17:26:40 +0100

    Spotvola adjustment and verification

--- 0_estimate_spotvolas.R
+++ 0_estimate_spotvolas.R
@@ -1,19 +1,20 @@
-# spot volatility estimation ----------------------------------------------
+# DO NOT RUN spot volatility estimation ----
+# Final output is available on repo spotvolas.rds
 library(tidyverse)
 library(highfrequency)
 
 source("_config.R")
 
-# load and prepare best bids data -----------------------------------------
+# DO NOT RUN load and prepare best bids data ----
 bids <- read_rds("data/best_bids_n_asks.rds") %>%
   select(exchange, ts, price = bid)
 
-## list of exchanges
+## DO NOT RUN list of exchanges
 exchanges <- bids %>%
   distinct(exchange) %>%
   .$exchange # 16 exchanges
 
-# estimate spotvolatility -------------------------------------------------
+# DO NOT RUN estimate spotvolatility ----
 ## function to compute spotvola 
 compute_spotvola <- function(j) {
   ## exchange-specific subsample of best bids
@@ -54,3 +55,35 @@ compute_spotvola <- function(j) {
 j <- as.integer(Sys.getenv("SGE_TASK_ID"))
 print(exchanges[j])
 compute_spotvola(j)
+
+source("_config.R")
+
+## aggregate exchange-level spotvola estimates
+spotvolas_files <- list.files("data/spotvolas", full.names = TRUE)
+
+spotvolas <- vector("list", length(spotvolas_files))
+for (j in 1:length(spotvolas_files)) {
+  spotvolas[[j]] <- read_rds(paste0(spotvolas_files[j]))
+}
+spotvolas <- bind_rows(spotvolas)
+
+## filter out 0 spotvolas and outliers
+spotvolas <- spotvolas %>%
+  filter(spotvola > 0) %>%
+  group_by(exchange) %>%
+  mutate(lb = quantile(spotvola, 0.01),
+         ub = quantile(spotvola, 0.99)) %>%
+  filter(spotvola > lb & spotvola < ub) %>%
+  ungroup() %>%
+  select(exchange, ts, spotvola)
+
+# Output available on repo ----
+write_rds(spotvolas, "data/spotvolas.rds")
+
+# DO NOT RUN summary stats ----
+
+spotvolas <- read_rds("data/spotvolas.rds")
+
+spotvolas %>% 
+  summarize(mean = 100 * mean(spotvola), 
+            daily_mean = sqrt(1440) * mean)
