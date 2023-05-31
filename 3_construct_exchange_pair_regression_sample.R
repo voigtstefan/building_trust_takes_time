bcf870826412fd2103346669217d82d49d2c4a7d
voigtstefan <stefan.voigt@econ.ku.dk>
2023-05-31 20:02:12 +0200

    update exchange pair regression

--- 3_construct_exchange_pair_regression_sample.R
+++ 3_construct_exchange_pair_regression_sample.R
@@ -2,40 +2,9 @@
 
 library(tidyverse)
 library(lubridate)
+library(primes)
 
 source("_config.R")
-
-# load data ---------------------------------------------------------------
-
-
-# Price differences 
-arbitrage <- read_rds("data/arbitrage_data.rds")
-arbitrage <- arbitrage |> 
-  head(1000000) |>
-  select(ts, buy_side, sell_side, delta) |> 
-  mutate(pair = map2(buy_side, sell_side,c), 
-         pair = map(pair, str_sort), 
-         pair = map_chr(pair, paste,collapse = "_"),
-         hour = floor_date(ts, "hour"))
-
-# TO DISCUSS: WHO IS SELL-SIDE EXCHANGE IF DELTA = 0? I removed the full-grid after all ..
-
-# Merge with arbitrage boundaries & sell-side spotvola
-arbitrage_boundaries <- read_rds("data/arbitrage_boundaries.rds")
-arbitrage_boundaries <- arbitrage_boundaries |> 
-  select(exchange, ts, spotvola, boundary = boundary_crra2)
-
-## Merge with spreads (in basis)
-best_bids_n_asks <- read_rds("data/best_bids_n_asks.rds")
-
-best_bids_n_asks <- best_bids_n_asks |> 
-  mutate(spread = 10000 * (ask - bid) / (ask / 2 + bid / 2)) |>
-  select(exchange, ts, spread)
-
-## Merge with parametrized latencies
-latencies_hourly <- read_rds("data/latencies_hourly.rds")
-latencies_hourly <- latencies_hourly |> 
-  select(hour = ts, median_latency, sd_latency)
 
 ## Merge with exchange characteristics
 exchange_characteristics <- read_csv("data/exchange_characteristics.csv") %>%
@@ -43,14 +12,87 @@ exchange_characteristics <- read_csv("data/exchange_characteristics.csv") %>%
   mutate(region = case_when(region == "UK" ~ "Europe", 
                             region == "Japan" ~ "Other", 
                             is.na(region) ~ "Other",
-                            TRUE ~ region))
+                            TRUE ~ region),
+         identifier = primes::generate_n_primes(n()))
+
+# Price differences 
+arbitrage <- read_rds("data/arbitrage_data.rds")
+
+arbitrage <- arbitrage |> 
+  left_join(exchange_characteristics |> select(exchange, identifier), by = c("sell_side" = "exchange")) |>
+  left_join(exchange_characteristics |> select(exchange, identifier), by = c("buy_side" = "exchange"), suffix = c(".sell",".buy")) |>
+  mutate(pair = identifier.sell * identifier.buy,
+         hour = floor_date(ts, "hour"),
+         delta = delta * 100) |> # note: scale returns to percent
+    select(ts, hour, buy_side, sell_side, pair, delta)
+  
+# Merge with arbitrage boundaries & sell-side spotvola
+arbitrage_boundaries <- read_rds("data/arbitrage_boundaries.rds")
+arbitrage_boundaries <- arbitrage_boundaries |> 
+  transmute(exchange, 
+            ts, 
+            spotvola = spotvola, 
+            boundary = boundary_crra2)
+
+## Merge with spreads (in basis)
+best_bids_n_asks <- read_rds("data/best_bids_n_asks.rds")
+
+best_bids_n_asks <- best_bids_n_asks |> 
+  mutate(spread = (ask - bid) / ask,
+         btc_price = (ask + bid) / 2) |>
+  select(exchange, ts, spread, btc_price)
+
+## Merge with parametrized latencies
+latencies_hourly <- read_rds("data/latencies_hourly.rds")
+latencies_hourly <- latencies_hourly |> 
+  select(hour = ts, latency_median = median_latency, latency_sd = sd_latency)
+
+## Merge with hourly inflows from other exchanges 
+flows_hourly <- read_rds("data/cross_exchange_flows.rds")
+
+flows_hourly <- flows_hourly |>
+  mutate(hour = floor_date(ts, "hour")) |>
+  group_by(from, to, hour) |>
+  summarize(volume = sum(volume * 1e-08)) |>
+  ungroup() |>
+  left_join(exchange_characteristics |> select(exchange, identifier), 
+            by = c("from" = "exchange")) |>
+  left_join(exchange_characteristics |> select(exchange, identifier), 
+            by = c("to" = "exchange"), suffix = c(".sell",".buy")) |>
+  mutate(pair = identifier.sell * identifier.buy) 
+
+# NOTE: I aggregate flows to / from on a pair/hour level to obtain net flows
+flows_hourly <- flows_hourly |>
+  drop_na(pair) |>
+  arrange(hour, pair) |>
+  group_by(pair, hour) |>
+  summarise(to = first(to),
+            from = first(from),
+            flow_volume = if_else(n() == 1, first(volume), first(volume) - last(volume))) |>
+  ungroup()
+  
+# Merge with (lagged) hourly exchange inventories 
+flows_and_balances <- read_rds("data/clean_flows_and_balances_hourly.rds") |>
+  select(hour = timestamp, sell_side = exchange, balance) |>
+  mutate(hour = hour - lubridate::days(1))
+
+# Merge all files
 
 regression_sample <- arbitrage |> 
   left_join(arbitrage_boundaries, by = c("sell_side" = "exchange", "ts")) |>
   left_join(best_bids_n_asks, by = c("sell_side" = "exchange", "ts")) |> 
   left_join(latencies_hourly, by = "hour") |>
-  left_join(exchange_characteristics, by = c("sell_side" = "exchange"))
+  left_join(exchange_characteristics |> select(-identifier), 
+            by = c("sell_side" = "exchange")) |>
+  left_join(exchange_characteristics |> select(-identifier), 
+            by = c("buy_side" = "exchange"), suffix = c(".sell",".buy")) 
 
+regression_sample <- regression_sample |>
+  left_join(flows_and_balances, by = c("sell_side", "hour")) |>
+  left_join(flows_hourly, by = c("pair", "hour")) |>
+  mutate(flow_volume = if_else(to == sell_side, flow_volume, -flow_volume)) |>
+  select(-from, -to)
+  
 # Prepare regression sample ---------
 
 trim <- function(x, cut) {
@@ -66,22 +108,47 @@ trim <- function(x, cut) {
 regression_sample_prepared <- regression_sample |> 
   # trim continuous outcome variables 
   mutate(across(
-    c(delta, spotvola, spread,
-      boundary),
+    c(delta, spotvola, spread, boundary),
     ~ trim(., 0.01)
   )) |>
-  drop_na(delta, spotvola, spread, boundary, median_latency, sd_latency) |> 
+  drop_na(delta, spotvola, spread, boundary, latency_median, latency_sd) |> 
   # gdax removed margin end of february 2018
-  mutate(margin_trading = if_else(sell_side == "gdax" & ts >= "2018-02-28", FALSE, as.logical(margin_trading))) |> 
+  mutate(margin_trading.sell = if_else(sell_side == "gdax" & ts >= "2018-02-28", FALSE, as.logical(margin_trading.sell)),
+         margin_trading.buy = if_else(buy_side == "gdax" & ts >= "2018-02-28", FALSE, as.logical(margin_trading.buy))) |> 
   mutate(
-    aa_rating = rating_categorial == "AA",
-    boundary_margin = margin_trading * boundary,
-    boundary_business = business_accounts * boundary,
-    latency_variance = sd_latency ^ 2,
-    latency_variance_std = scale(latency_variance)
+    aa_rating.buy = rating_categorial.buy == "AA",
+    aa_rating.sell = rating_categorial.sell == "AA",
+    boundary_margin = margin_trading.sell * boundary,
+    boundary_business = business_accounts.sell * boundary,
+    latency_variance = latency_sd ^ 2,
+    latency_variance_std = scale(latency_variance),
+    balance = replace_na(balance, 0),
+    flow_volume = flow_volume * btc_price / 100000
+  ) |>
+  select(-btc_price)
+
+# Summary statistics
+regression_sample_prepared |> 
+  select(
+    delta, spotvola, median_latency, sd_latency, boundary, 
+    spread, margin_trading.sell, business_accounts.sell
+  ) |> 
+  pivot_longer(cols = everything()) |> 
+  drop_na() |>
+  group_by(name) |>
+  summarize(
+    mean = mean(value),
+    sd = sd(value),
+    min = min(value),
+    q05 = quantile(value, 0.05),
+    q50 = quantile(value, 0.50),
+    q95 = quantile(value, 0.95),
+    max = max(value),
+    n = n()
   )
 
 # store regression sample
 
-write_csv(regression_sample, "data/exchange_pair_regression_sample.csv.gz")
+write_rds(regression_sample_prepared, "data/exchange_pair_regression_sample.rds",
+          compress = "gz")
 
--- 4_exchange_pair_regressions.R
+++ 4_exchange_pair_regressions.R
@@ -4,58 +4,7 @@ library(tidyverse)
 library(fixest)
 
 source("_config.R")
-regression_sample <- read_csv("data/exchange_pair_regression_sample.csv")
-
-# Prepare regression sample -----------------------------------------------
-trim <- function(x, cut) {
-  x <- replace(
-    x, x > quantile(x, 1 - cut, na.rm = T), NA
-  )
-  x <- replace(
-    x, x < quantile(x, cut, na.rm = T), NA
-  )
-  return(x)
-}
-
-regression_sample_prepared <- regression_sample |> 
-  # trim continuous outcome variables 
-  mutate(across(
-    c(delta, spotvola, spread,
-      boundary),
-    ~ trim(., 0.01)
-  )) |>
-  drop_na(delta, spotvola, spread, boundary, median_latency, sd_latency) |> 
-  # gdax removed margin end of february 2018
-  mutate(margin_trading = if_else(sell_side == "gdax" & ts >= "2018-02-28", FALSE, as.logical(margin_trading))) |> 
-  # replace missing number of confirmations
-  mutate(
-    aa_rating = rating_categorial == "AA",
-    boundary_margin = margin_trading * boundary,
-    boundary_business = business_accounts * boundary,
-    latency_variance = sd_latency ^ 2,
-    latency_variance_std = scale(latency_variance)
-  )
-
-# Summary statistics ------------------------------------------------------
-regression_sample_prepared |> 
-  select(
-    delta, spotvola, median_latency, sd_latency, boundary, 
-    spread, margin_trading, 
-    business_accounts
-  ) |> 
-  pivot_longer(cols = everything()) |> 
-  drop_na() |>
-  group_by(name) |>
-  summarize(
-    mean = mean(value),
-    sd = sd(value),
-    min = min(value),
-    q05 = quantile(value, 0.05),
-    q50 = quantile(value, 0.50),
-    q95 = quantile(value, 0.95),
-    max = max(value),
-    n = n()
-  )
+regression_sample <- read_rds("data/exchange_pair_regression_sample.rds")
 
 # label variables
 dictionary <- c(
@@ -75,33 +24,45 @@ dictionary <- c(
 
 # Price Differences and Sources of Price Risk -----------------------------
 vcov <- "hetero"
+
 pd_model1 <- feols(
   delta ~ boundary + spread | pair,
-  vcov = vcov,
-  data = regression_sample_prepared
+  vcov = ~pair,
+  data = regression_sample 
 )
 
 pd_model2 <- feols(
-  delta ~ spotvola + median_latency + latency_variance_std + spread | pair,
-  vcov = vcov,
-  data = regression_sample_prepared
+  delta ~ spotvola + latency_median + latency_variance_std + spread | pair,
+  vcov = ~pair,
+  data = regression_sample 
 )
 
 pd_model3 <- feols(
   delta ~ boundary + boundary_margin + spread | pair,
-  vcov = vcov,
-  data = regression_sample_prepared
+  vcov = ~pair,
+  data = regression_sample 
 )
 
 pd_model4 <- feols(
   delta ~ boundary + boundary_business + spread | pair,
-  vcov = vcov,
-  data = regression_sample_prepared
+  vcov = ~pair,
+  data = regression_sample 
 )
 
+pd_model5 <- feols(
+  delta ~ boundary + balance + spread | pair,
+  vcov = ~pair,
+  data = regression_sample
+)
+
+pd_model6 <- feols(
+  delta ~  spotvola + latency_median + latency_variance_std + balance + spread | pair,
+  vcov = ~pair,
+  data = regression_sample
+)
 
 etable(
-  pd_model1, pd_model2, pd_model3, pd_model4,
+  pd_model1, pd_model2, pd_model3, pd_model4, pd_model5, pd_model6,
   coefstat = "tstat",
   dict = dictionary
 )
@@ -116,116 +77,3 @@ etable(
   fontsize = "footnotesize",
   tabular = "X",
   notes = "\\emph{Notes: }")
-
-
-# Robustness check: USDT and regional splits ---------------------------------------
-pd_model7 <- feols(
-  delta_regional ~ boundary + log_balance + spread | sell_side,
-  vcov = vcov,
-  data = regression_sample_prepared |> 
-    filter(region == "USA")
-)
-
-pd_model8 <- feols(
-  delta_regional ~  boundary + log_balance + spread | sell_side,
-  vcov = vcov,
-  data = regression_sample_prepared |> 
-    filter(region == "Europe")
-)
-
-pd_model9 <- feols(
-  delta_regional ~  boundary + log_balance + spread | sell_side,
-  vcov = vcov,
-  data = regression_sample_prepared
-)
-
-pd_model10 <- feols(
-  delta_tether ~ boundary + log_balance + spread | sell_side,
-  vcov = vcov,
-  data = regression_sample_prepared |> 
-    filter(tether == TRUE)
-)
-
-pd_model11 <- feols(
-  delta_tether ~ boundary + log_balance + spread | sell_side,
-  vcov = vcov,
-  data = regression_sample_prepared |> 
-    filter(tether == FALSE)
-)
-
-pd_model12 <- feols(
-  delta_tether ~ boundary + log_balance + spread | sell_side,
-  vcov = vcov,
-  data = regression_sample_prepared
-)
-
-pd_model13 <- feols(
-  delta ~  boundary*aa_rating + log_balance + spread | sell_side,
-  vcov = vcov,
-  data = regression_sample_prepared
-)
-
-etable(
-  pd_model13, pd_model7, pd_model8, pd_model9, pd_model10, pd_model11, pd_model12,  
-  coefstat = "tstat",
-  dict = dictionary
-)
-
-etable(
-  pd_model13, pd_model7, pd_model8, pd_model9, pd_model10, pd_model11, pd_model12,  
-  coefstat = "tstat",
-  dict = dictionary, 
-  tex = TRUE, 
-  style.tex = style.tex(line.top = "\\toprule", line.bottom = "\\bottomrule"), 
-  headers = list("^:_:\\emph{Dependent Variable}" = c("Price Differences (in %)", 
-                                                      "Regional Price Differences (in %)", "Regional Price Differences (in %)", "Regional Price Differences (in %)",
-                                                      "Price Differences (in %)", "Price Differences (in %)", "Price Differences (in %)"),
-                 "Subset" = c("", "USA", "Europe", "All", "USDT", "USD", "All")), 
-  fitstat=c('n'),
-  depvar = FALSE,
-  group=list("Controls: Inventory and Spread" = c("Spread", "Inventory")))
-
-# Cross-Exchange Flows and Arbitrage Opportunities ------------------------
-flows_model1 <- feols(
-  inflows ~ spread | sell_side| delta ~ spotvola +  median_latency + sd_latency,
-  vcov = vcov,
-  data = regression_sample_prepared
-)
-
-flows_model2 <- feols(
-  inflows ~  spread | sell_side| delta ~ boundary,
-  vcov = vcov,
-  data = regression_sample_prepared
-)
-
-flows_model3 <- feols(
-  log_inflows ~ spread | sell_side| delta ~ spotvola +  median_latency + sd_latency,
-  vcov = vcov,
-  data = regression_sample_prepared
-)
-
-flows_model4 <- feols(
-  log_inflows ~  spread | sell_side| delta ~ boundary,
-  vcov = vcov,
-  data = regression_sample_prepared
-)
-
-etable(
-  flows_model1, flows_model2, flows_model3, flows_model4, 
-  coefstat = "tstat",
-  dict = dictionary
-)
-
-# Tex output (requires some manual styling)
-etable(
-  flows_model1, flows_model2, flows_model3, flows_model4, 
-  coefstat = "tstat",
-  dict = dictionary, 
-  tex = TRUE, 
-  style.tex = style.tex(line.top = "\\toprule", line.bottom = "\\bottomrule"), 
-  headers = list("^:_:\\emph{Dependent Variable}" = c("Exchange Inflows (in 100k USD)", "Exchange Inflows (in 100k USD)", "Log(Exchange Inflows)", "Log(Exchange Inflows)")), 
-  fitstat=c('n'),
-  depvar = FALSE)
-
-
-
